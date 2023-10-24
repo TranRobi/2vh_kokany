b2edc933cdd9637fb10f0e9dff0a75cf6cb18e3c
TranRobi <dtranduy@gmail.com>
2023-10-24 11:04:29 +0200

    Projekt vége

--- src/Compononents/Feladatok/Feladat.js
+++ src/Compononents/Feladatok/Feladat.js
@@ -2,9 +2,26 @@ import React, { useState } from "react";
 import Data from "../../Data/Feladatok.json";
 
 import "./Feladat.css";
+import { Link } from "react-router-dom";
 
 function Feladat() {
 	const [Jelenlegi, setJelenlegi] = useState(0);
+	const [Pontok, setPontok] = useState(0);
+
+	const handleKov = (boolean) => {
+		const koviKerdes = Jelenlegi + 1;
+
+		if (boolean === true) {
+			setPontok(Pontok + 1);
+		}
+
+		if (koviKerdes < Data.length) {
+			setJelenlegi(koviKerdes);
+			return;
+		} else {
+			alert("Vége a quiznek");
+		}
+	};
 
 	return (
 		<>
@@ -14,6 +31,26 @@ function Feladat() {
 					<h2>
 						Kérdések {Data[Jelenlegi].id} / {Data.length}
 					</h2>
+					<h2>
+						Pontjaid: {Pontok} / {Data.length}
+					</h2>
+				</div>
+				<div className="valaszok">
+					{Data[Jelenlegi].valaszok.map((valasz) => {
+						return (
+							<div>
+								<button
+									className="valasz"
+									onClick={() => handleKov(valasz.helyesE)}
+								>
+									{valasz.valasz}
+								</button>
+							</div>
+						);
+					})}
+				</div>
+				<div>
+					<Link to="/">Vissza a kezdőlapra</Link>
 				</div>
 			</div>
 		</>
--- src/Compononents/Home/Home.js
+++ src/Compononents/Home/Home.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import "./Home.css"
+import "./Home.css";
 
 function Home() {
-  return (
-    <div className='fooldal'>
-        <h1>2. Világháború</h1>
-        
-    </div>
-  )
+	return (
+		<div className="fooldal">
+			<h1>2. Világháború</h1>
+			<h2>
+				Ezen a weboldalon amit a Kókány Corporations csapata készített a második
+				Világháborúról tudtok tanulni illetve feladatokat megoldani
+			</h2>
+			<h2>A csapat tagjai: Mester Máté, Tran Duy Dat, Vadász Zsolt</h2>
+			<h2>
+				A project elkészítési ideje: 4 nap Szombattól Keddig, Napi 3 óra
+				foglalkozás
+			</h2>
+			<h2>A weboldal elkészítéséhez React Js, CSS, HTML volt szükséges.</h2>
+			<h2>Az egész project fent megtalálható a github repónkban</h2>
+			<Link to="https://github.com/TranRobi/2vh_kokany" target="_blank">
+				Github repo
+			</Link>
+		</div>
+	);
 }
 
-export default Home
+export default Home;
